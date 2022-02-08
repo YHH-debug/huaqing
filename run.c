afe63835b85acf71d7c6833f4aaa9f634da159d0
YHH-debug <2353914883@qq.com>
2022-02-08 15:19:20 +0800

    ddd

---
+++ run.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: run.c
+	> Author: YHH
+	> Mail: 2353914883@qq.com
+	> Created Time: Tue 08 Feb 2022 03:18:32 PM CST
+ ************************************************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
+
